67ba2d022b76f1dcea53099407de873adbe0ae12
HazeHacker <3542495583@qq.com>
2026-01-12 09:54:24 +0800

    fix: 修复超时取消的逻辑

--- backend/order-service/src/main/java/top/hazenix/order/listener/DelayListener.java
+++ backend/order-service/src/main/java/top/hazenix/order/listener/DelayListener.java
@@ -28,10 +28,19 @@ public class DelayListener {
     ))
     public void listenDelayMessage(Message message) throws Exception {
         log.info("接收到delay.queue的延迟消息：{}", new String(message.getBody()));
-        // 检查订单是否状态
-        // 如果未支付，则取消订单
         String orderId = new String(message.getBody());
         Orders order = orderMapper.getById(Long.valueOf(orderId));
+        // 前置校验
+        if (order == null || order.getStatus() != Orders.PENDING_PAYMENT){
+            // 订单不存在 / 已经支付，无需再处理
+            return;
+        }
+        // 订单表支付状态为未支付，接下来查询支付流水状态(支付订单表如果有)
+        // 如果有支付订单这张表，先去支付订单表查询 该订单支付状态
+        // 如果支付状态为已支付，就将订单表的状态也改为已支付（可能是因为消息没发送到导致没同步）
+        // 如果支付状态为未支付，则取消订单
+
+        // 订单存在，如果未支付，则取消订单(恢复库存 (如果有库存这个逻辑))
         if(order.getPayStatus() == Orders.UN_PAID){
             orderService.cancel(order.getId());
         }
